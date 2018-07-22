cb5302a2695ce6c26afadd5bbc343ba777bfafff
Anne-Christine Lefort <zanskar49@yahoo.fr>
2018-07-22 23:39:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/inventory/CatalogActivity.java
@@ -0,0 +1,185 @@
+package com.example.android.inventory;
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.support.design.widget.FloatingActionButton;
+
+import com.example.android.inventory.data.ProductContract;
+import com.example.android.inventory.data.ProductContract.ProductEntry;
+import com.example.android.inventory.data.ProductProvider;
+import com.example.android.pets.R;
+
+/**
+ * Displays list of products that were entered and stored in the app.
+ */
+public class CatalogActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+    /** Tag for the log messages */
+    public static final String LOG_TAG = ProductProvider.class.getSimpleName();
+
+    /** Identifier for the product data loader */
+    private static final int PRODUCT_LOADER = 0;
+
+    /** Adapter for the ListView */
+    ProductCursorAdapter mCursorAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Find the ListView which will be populated with the product data
+        ListView productListView = (ListView) findViewById(R.id.list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        productListView.setEmptyView(emptyView);
+
+        // Setup an Adapter to create a list item for each row of product data in the Cursor.
+        // There is no product data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new ProductCursorAdapter(this, null);
+        productListView.setAdapter(mCursorAdapter);
+
+        // Setup the item click listener
+        productListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Create new intent to go to {@link EditorActivity}
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+
+                // Form the content URI that represents the specific product that was clicked on,
+                // by appending the "id" (passed as input to this method) onto the
+                // {@link ProductEntry#CONTENT_URI}.
+                // For example, the URI would be "content://com.example.android.products/products/2"
+                // if the product with ID 2 was clicked on.
+                Uri currentProductUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentProductUri);
+
+                // Launch the {@link EditorActivity} to display the data for the current product.
+                startActivity(intent);
+            }
+        });
+
+        // Kick off the loader
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
+    }
+    /**
+     * Helper method to insert hardcoded pet data into the database. For debugging purposes only.
+     */
+    private void insertProduct() {
+    // Create a ContentValues object where column names are the keys,
+    // and my product test attributes are the dummy values.
+    ContentValues values = new ContentValues();
+        values.put(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME, "Mon livre");
+        values.put(ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE, 10);
+        values.put(ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, 1);
+        values.put(ProductContract.ProductEntry.COLUMN_SUPPLIER_NAME, "Mr X");
+        values.put(ProductContract.ProductEntry.COLUMN_SUPPLIER_PHONE, "56 00 56 00");
+
+    // Insert a new row for my product test in the database, returning the ID of that new row.
+    // The first argument for db.insert() is the products table name.
+    // The second argument provides the name of a column in which the framework
+    // can insert NULL in the event that the ContentValues is empty (if
+    // this is set to "null", then the framework will not insert a row when
+    // there are no values).
+    // The third argument is the ContentValues object containing the info for my product test.
+
+    // Use the {@link ProductEntry#CONTENT_URI} to indicate that we want to insert
+    // into the products database table.
+    // Receive the new content URI that will allow us to access "Mon livre" data in the future.
+
+    Uri newUri = getContentResolver().insert(ProductEntry.CONTENT_URI, values);
+
+        Log.e(LOG_TAG, "dummy values entered");
+    }
+    /**
+     * Helper method to delete all products in the database.
+     */
+    private void deleteAllProducts() {
+        int rowsDeleted = getContentResolver().delete(ProductEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from product database");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                insertProduct();
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                deleteAllProducts();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+
+    // Define a projection that specifies which columns from the database
+            // you will actually use after this query.
+            String[] projection = {
+            ProductContract.ProductEntry._ID,
+            ProductContract.ProductEntry.COLUMN_PRODUCT_NAME,
+            ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE,
+            ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY,
+            ProductContract.ProductEntry.COLUMN_SUPPLIER_NAME,
+            ProductContract.ProductEntry.COLUMN_SUPPLIER_PHONE,
+    };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                ProductEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            // Update {@link Product CursorAdapter} with this new cursor containing updated product data
+            mCursorAdapter.swapCursor(data);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            // Callback called when the data needs to be deleted
+            mCursorAdapter.swapCursor(null);
+        }
+}
---
+++ app/src/main/java/com/example/android/inventory/ProductCursorAdapter.java
@@ -0,0 +1,78 @@
+package com.example.android.inventory;
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.inventory.data.ProductContract;
+import com.example.android.pets.R;
+
+
+/**
+        * {@link ProductCursorAdapter} is an adapter for a list or grid view
+        * that uses a {@link Cursor} of product data as its data source. This adapter knows
+        * how to create list items for each row of product data in the {@link Cursor}.
+        */
+public class ProductCursorAdapter extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link ProductCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public ProductCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Inflate a list item view using the layout specified in list_item.xml
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the product data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current product can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find individual views that we want to modify in the list item layout
+        TextView nameTextView = (TextView) view.findViewById(R.id.name);
+        TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
+
+        // Find the columns of products attributes that we're interested in
+        int productNameColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
+
+        // Read the products attributes from the Cursor for the current product
+        String productName = cursor.getString(productNameColumnIndex);
+        String price = cursor.getString(priceColumnIndex);
+        String quantity = cursor.getString(quantityColumnIndex);
+
+        // Update the TextViews with the attributes for the current product
+        nameTextView.setText(productName);
+        summaryTextView.setText(price);
+        summaryTextView.setText(quantity);
+    }
+}
+
