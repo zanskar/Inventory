34db0c00b771739bf11fc29753bc1a6e9cf220da
Anne-Christine Lefort <zanskar49@yahoo.fr>
2018-08-04 23:40:02 +0200

    Corrections reviews 1 - changes to make + and - buttons to save data into database but not working

--- app/src/main/java/com/example/android/inventory/EditorActivity.java
+++ app/src/main/java/com/example/android/inventory/EditorActivity.java
@@ -144,7 +144,7 @@ public class EditorActivity extends AppCompatActivity implements
             public void onClick(View view) {
                 int newQuantity = Integer.parseInt(mQuantityEditText.getText().toString().trim());
                 mQuantityEditText.setText(String.valueOf(newQuantity + 1));
-                Uri newUri = ContentUris.withAppendedId(ProductContract.ProductEntry.CONTENT_URI, _ID);
+                Uri newUri = ContentUris.withAppendedId(ProductContract.ProductEntry.CONTENT_URI, id);
                 ContentValues values = new ContentValues();
                 values.put(ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, newQuantity + 1);
                 mContext.getContentResolver().update(newUri, values, null, null);
